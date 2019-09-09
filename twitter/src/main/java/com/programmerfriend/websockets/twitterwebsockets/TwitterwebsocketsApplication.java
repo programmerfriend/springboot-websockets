c54bde2d07353102105526fdf787118b0f075940
eiselems <marcus.eisele@gmail.com>
2019-09-10 00:49:43 +0200

    INIT from start.spring.io

---
+++ twitter/src/main/java/com/programmerfriend/websockets/twitterwebsockets/TwitterwebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.programmerfriend.websockets.twitterwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterwebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterwebsocketsApplication.class, args);
+	}
+
+}
